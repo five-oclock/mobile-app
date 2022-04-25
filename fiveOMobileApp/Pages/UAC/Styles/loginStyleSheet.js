46f5a540a0de99bfaaa6b986b4534cf9d325ed94
Izzy Russo <isabel.russo@colorado.edu>
2022-04-25 17:21:46 -0600

    Logo on login screen

---
+++ fiveOMobileApp/Pages/UAC/Styles/loginStyleSheet.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native"
+// Orange: BD8334
+// Blue: 1D8EB6
+// Light Gray: B1AEA9
+// Dark Gray: 746D65
+// Gold: DCBD7C
+
+export default StyleSheet.create({
+
+    //Login Page
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      tinyLogo: {
+        width: 50,
+        height: 50,
+      },
+      logo: {
+        width: 250 ,
+        height: 250,
+        
+      },
+});
